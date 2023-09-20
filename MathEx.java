e51442a772f77fb5749d5131d0f0a80e34ac33af
YP10 <yashpatel@billericak12.com>
2023-09-20 15:04:16 -0400

    Swap

--- MathEx.java
+++ MathEx.java
@@ -1,8 +1,10 @@
-public class MathEx {
-    Swap(123);
-    
-    public static int Swap(int z){
-        int result = z/100*100+z%10*10+z%100/10
-        return result;
+public class MathEx {   
+    public static void main(String [] args){
+        Swap(579);
+        Swap(173);
+        Swap(432);
+    }
+    public static void Swap(int z){
+        System.out.println(z/100*100+z%10*10+z%100/10);
     }
 }
