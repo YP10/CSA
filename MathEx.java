67bfa481adc38aea7d79881fc14077e1b3fc193b
YP10 <yashpatel@billericak12.com>
2023-09-20 19:08:01 -0400

    MathEx

--- MathEx.java
+++ MathEx.java
@@ -1,12 +1,21 @@
 public class MathEx {   
     public static void main(String [] args){
-        
+        Swap();
     }
-    public static void print(int y){
+    public static void Swap(){
+        int y= Swap1(465);
         System.out.println(y);
+        int x= Swap1 (457);
+        System.out.println(x);
+        int w = Swap1(792);
+        System.out.println(w);
+
     }
-    public static int Swap(int z){
+    public static int Swap1(int z){
         int result = z/100*100+z%10*10+z%100/10;
         return result;
     }
 }
+
+
+
