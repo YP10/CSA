99ebca418cb9c10d1eabdfa1749ce96379f0004b
YP10 <yashpatel@billericak12.com>
2023-09-22 22:15:14 -0400

    MathEx

--- MathEx.java
+++ MathEx.java
@@ -1,21 +1,42 @@
-public class MathEx {   
+public class MathEx {  
     public static void main(String [] args){
-        Swap();
-    }
-    public static void Swap(){
-        int y= Swap1(465);
+         int y= swap(346);
         System.out.println(y);
-        int x= Swap1 (457);
+        int x= swap (785);
         System.out.println(x);
-        int w = Swap1(792);
+        int w = swap(216);
         System.out.println(w);
-
+        timeLeft(3,53,8,47);
+        timeLeft(2,24,7,58);
+        dayOfWeek(6,23);
+        dayOfWeek(3,17);
+       
     }
-    public static int Swap1(int z){
+    public static int swap(int z){
         int result = z/100*100+z%10*10+z%100/10;
         return result;
     }
-}
+    public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
+        int x = (depHour*60+depMin)- (curHour*60+curMin);
+        int y = x/60;
+        int z = x%60;
+        System.out.println( y+" hours "+ z+ " minutes");
+    }    
+    public static void dayOfWeek(int day, int date){
+            int x= (date%7);
+            int y= ((day+x)+7)-1;
+            System.out.println(y);
+    }
+
+
+    }
+
+        
+    
+
+
+
+
 
 
 
