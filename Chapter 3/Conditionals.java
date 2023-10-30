222a90c8780a5f60143061a35cb2aae9112adbad
YP10 <yashpael@billericak12.com>
2023-10-30 10:23:01 -0400

    Conditionals

--- Chapter 3/Conditionals.java
+++ Chapter 3/Conditionals.java
@@ -1,3 +1,9 @@
 public class Conditionals {
-    
+    public boolean chimpTrouble(boolean aSmile, boolean bSmile){
+        if(aSmile==bSmile) return true;
+        else return false;
+    }   
+    public boolean negPos(int a, int b){
+        
+    }
 }
