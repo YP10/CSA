247a75e9803bb59d8b260fc0e797ec158db6dea8
YP10 <yashpatel@billericak12.com>
2023-11-20 16:57:57 -0500

    Craps

---
+++ Chapter 3/Die.java
@@ -0,0 +1,16 @@
+public class Die {
+        int getNumDots0;
+    public void roll(){
+        getNumDots0 = (int)(Math.random()*6)+1;
+    }
+    public int getNumDots(){
+        return getNumDots0;
+    }
+    
+    public static void main(String[] args) {
+        Die die = new Die();
+        die.roll();
+       System.out.println(die.getNumDots());
+
+    }
+}
