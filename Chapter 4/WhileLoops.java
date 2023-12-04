3dddd55a6263db965792901de31bd37c7c6bc4c0
YP10 <yashpatel@billericak12.com>
2023-12-04 09:17:18 -0500

    Chapters

--- Chapter 4/WhileLoops.java
+++ Chapter 4/WhileLoops.java
@@ -1,3 +1,15 @@
 public class WhileLoops{
-    
+    public int addOdds(int n){
+        int i= -1;
+        int sum=0;
+        while(i<=n){
+            i+=2;
+            sum+=i;
+        }
+        return sum;
+    }
+    public static void main(String [] args){
+        WhileLoops w= new WhileLoops();
+        System.out.println(w.addOdds(7));
+    }
 }
