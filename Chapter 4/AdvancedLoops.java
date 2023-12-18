27f7e0d43cea85633322a57a1b6b2bf5a658cc57
YP10 <yashpatel@billericak12.com>
2023-12-18 10:21:53 -0500

    Uhhhhhhh

---
+++ Chapter 4/AdvancedLoops.java
@@ -0,0 +1,58 @@
+public class AdvancedLoops {
+    public void eights(){
+        for(int j=8; j>=2; j-=2){
+             for(int k=8;k>j;k-=2){
+                System.out.print(" ");
+            }
+            for(int i=1;i<=j;i++){
+                System.out.print(j);
+            }
+        System.out.println();
+        }
+    }
+    public void alternate(int x){
+        for(int i=1;i<=x;i++){
+            if(i%2==0){
+                for(int j=i;j>0;j--){
+                        System.out.print(j);
+                    }
+                    System.out.println();
+                }
+            if(i%2!=0){
+                for(int a=1;a<=i;a++){
+                    System.out.print(a);
+                }
+                System.out.println();
+            }
+        }
+    }
+    public void isosceles(int x){
+        for(int i=1;i<=x;i++){
+            for(int k=1;k<x-i+1;k++){
+                System.out.print(" ");
+            } 
+            for(int j=1;j<i;j++){
+                System.out.print(j);
+            }
+            for(int j=i;j>0;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public void upSideDown(int x, int y){
+        for(int i=1;i<x-y+1;i++){
+            
+        }
+    }
+    public static void main(String [] args){
+        AdvancedLoops t= new AdvancedLoops();
+        t.eights();
+        System.out.println("Alternate");
+        t.alternate(5);
+        t.alternate(6);
+        System.out.println("Isosceles");
+        t.isosceles(5);
+        t.isosceles(4);
+    }
+}
