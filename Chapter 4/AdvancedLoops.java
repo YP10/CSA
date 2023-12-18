e1efb86672f71a4c630e593d167601906bdde573
YP10 P <yashpatel@billericak12.com>
2023-12-18 17:09:09 +0000

    Done

--- Chapter 4/AdvancedLoops.java
+++ Chapter 4/AdvancedLoops.java
@@ -40,10 +40,21 @@ public class AdvancedLoops {
             System.out.println();
         }
     }
+    
     public void upSideDown(int x, int y){
-        for(int i=1;i<x-y+1;i++){
-            
+        for(int i=x;i>=y;i--){
+            for(int b=1;b<=x-i;b++){ 
+                System.out.print(" "); 
+            }
+            for(int j=i;j>y;j--){
+                System.out.print(j);
+            }
+            for(int a=y;a<=i;a++){
+                System.out.print(a);
+            }
+            System.out.println();
         }
+
     }
     public static void main(String [] args){
         AdvancedLoops t= new AdvancedLoops();
@@ -54,5 +65,7 @@ public class AdvancedLoops {
         System.out.println("Isosceles");
         t.isosceles(5);
         t.isosceles(4);
+        System.out.println("Upside Down");
+        t.upSideDown(9, 3);
     }
 }
